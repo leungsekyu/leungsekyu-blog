e08a1e0bd8793f503879cacf36238db846653fca
leungsekyu <leungsekyu@qq.com>
2025-01-10 07:20:06 +0800

    配置了 sitemap v1.0 ✅

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,14 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import UnoCSS from "unocss/astro";
+import unocss from "unocss/astro";
 
 export default defineConfig({
-  // used to generate images
-  site:
-    process.env.VERCEL_ENV === "production"
-      ? "https://brutal.elian.codes/"
-      : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}/`
-        : "https://localhost:3000/",
-  trailingSlash: "ignore",
-  integrations: [sitemap(), UnoCSS({ injectReset: true })],
+  site: "https://leungsekyu.com",
+  integrations: [sitemap(), unocss({ injectReset: true })],
   vite: {
     resolve: {
       alias: {
-        "@icons": "/public/icons", // 为 SCSS 配置别名
+        "@icons": "/public/icons",
       },
     },
     optimizeDeps: {

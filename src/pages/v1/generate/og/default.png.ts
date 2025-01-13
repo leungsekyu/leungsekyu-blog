90f78b85cbe0c9ef889eab84417e25caec02cfb4
leungsekyu <leungsekyu@qq.com>
2025-01-14 01:45:49 +0800

    修改了域名

--- src/pages/v1/generate/og/default.png.ts
+++ src/pages/v1/generate/og/default.png.ts
@@ -1,19 +1,19 @@
-import { Resvg, type ResvgRenderOptions } from '@resvg/resvg-js';
-import type { APIRoute } from 'astro';
-import satori from 'satori';
-import { html as toReactElement } from 'satori-html';
+import { Resvg, type ResvgRenderOptions } from '@resvg/resvg-js'
+import type { APIRoute } from 'astro'
+import satori from 'satori'
+import { html as toReactElement } from 'satori-html'
 
 const fontFile = await fetch(
-  'https://og-playground.vercel.app/inter-latin-ext-700-normal.woff'
-);
+  'https://og-playground.vercel.app/inter-latin-ext-700-normal.woff',
+)
 
-const fontData: ArrayBuffer = await fontFile.arrayBuffer();
+const fontData: ArrayBuffer = await fontFile.arrayBuffer()
 
-const height = 630;
-const width = 1200;
+const height = 630
+const width = 1200
 
 export const GET: APIRoute = async () => {
-  const link = 'https://brutal.elian.codes';
+  const link = 'https://www.leungsekyu.com'
   const html = toReactElement(`
   <div style="background-color: white; display: flex; flex-direction: column; height: 100%; padding: 3rem; width: 100%">
     <div style="display:flex; height: 100%; width: 100%; background-color: white; border: 6px solid black; border-radius: 0.5rem; padding: 2rem; filter: drop-shadow(6px 6px 0 rgb(0 0 0 / 1));">
@@ -25,12 +25,12 @@ export const GET: APIRoute = async () => {
         </div>
         <div style="display: flex; justify-content: space-between; align-items: baseline; padding-top: -2rem;">
           <p style="font-size: 32px">${link}</p>
-          <img src="https://www.elian.codes/assets/img/elian.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
+          <img src="https://www.leungsekyu.com/avatar/leungsekyu.jpg" width="200px" height="200px" style="border: 3px solid black; border-radius: 0.5rem;" />
         </div>
       </div>
     </div>
   </div>
-  `);
+  `)
 
   const svg = await satori(html, {
     fonts: [
@@ -43,21 +43,21 @@ export const GET: APIRoute = async () => {
 
     height,
     width,
-  });
+  })
 
   const opts: ResvgRenderOptions = {
     fitTo: {
       mode: 'width',
       value: width,
     },
-  };
-  const resvg = new Resvg(svg, opts);
-  const pngData = resvg.render();
-  const pngBuffer = pngData.asPng();
+  }
+  const resvg = new Resvg(svg, opts)
+  const pngData = resvg.render()
+  const pngBuffer = pngData.asPng()
 
   return new Response(pngBuffer, {
     headers: {
       'content-type': 'image/png',
     },
-  });
-};
+  })
+}

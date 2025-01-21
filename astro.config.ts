aaf3518c6d015d5b8ae9cad1f1254d120de75194
leungsekyu <leungsekyu@qq.com>
2025-01-21 15:34:14 +0800

    增加了对 MDX，Mermaid 的支持

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import unocss from 'unocss/astro'
 
+import mdx from '@astrojs/mdx'
+
+import remarkMermaid from 'remark-mermaidjs'
+
 export default defineConfig({
   site: 'https://leungsekyu.netlify.app/',
-  integrations: [sitemap(), unocss({ injectReset: true })],
+  integrations: [sitemap(), unocss({ injectReset: true }), mdx()],
   vite: {
     resolve: {
       alias: {
@@ -14,5 +18,20 @@ export default defineConfig({
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
     },
+  },
+  markdown: {
+    // 启用 remark 插件
+    remarkPlugins: [
+      [
+        remarkMermaid,
+        {
+          // 插件的配置选项（可选）
+          theme: 'default', // Mermaid 主题 (default, dark, forest, etc.)
+          themeVariables: {
+            primaryColor: '#ffcc00', // 自定义主题颜色
+          },
+        },
+      ],
+    ],
   },
 })

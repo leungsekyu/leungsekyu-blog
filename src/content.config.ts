86c9653c173cb2930584424ecee4637520505bdb
leungsekyu <leungsekyu@qq.com>
2025-01-23 03:39:56 +0800

    移除了注释

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,11 @@
 import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
-// 定义博客集合的配置
 const blog = defineCollection({
-  // 使用 glob 加载器来匹配博客文件
-  // pattern: 匹配所有非下划线开头的 .md 文件
-  // base: 指定博客文件的基础目录
   loader: glob({
     pattern: '**/[^_]*.{md,mdx}',
     base: './src/content/blog',
   }),
-
-  // 定义博客文章的数据模式
   schema: ({ image }) =>
     z.object({
       title: z.string(),

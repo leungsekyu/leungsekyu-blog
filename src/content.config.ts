b7b5b7169612c0ab074138e5687b1b9a9399e0a6
leungsekyu <leungsekyu@qq.com>
2025-01-23 02:27:56 +0800

    删除了注释并提高了代码可读性

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 // 定义博客集合的配置
-const blogCollection = defineCollection({
+const blog = defineCollection({
   // 使用 glob 加载器来匹配博客文件
   // pattern: 匹配所有非下划线开头的 .md 文件
   // base: 指定博客文件的基础目录
@@ -14,19 +14,9 @@ const blogCollection = defineCollection({
   // 定义博客文章的数据模式
   schema: ({ image }) =>
     z.object({
-      // 文章标题
       title: z.string(),
-
-      // 作者名称
-      author: z.string(),
-
-      // 文章标签数组
-      tags: z.array(z.string()),
-
-      // 文章描述
       description: z.string(),
-
-      // 发布日期 - 将字符串转换为 Date 对象
+      author: z.string().optional(),
       publishedAt: z
         .string()
         .transform((str) => new Date(str))
@@ -35,13 +25,9 @@ const blogCollection = defineCollection({
         .string()
         .transform((str) => new Date(str))
         .optional(),
-
-      // 文章封面图片
+      tags: z.array(z.string()),
       imgUrl: image(),
     }),
 })
 
-// 导出所有内容集合
-export const collections = {
-  blog: blogCollection,
-}
+export const collections = { blog }

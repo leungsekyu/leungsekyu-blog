5cbbd5c1127b14c419039bad91cd42d6d1127dba
leungsekyu <leungsekyu@qq.com>
2025-02-02 00:11:54 +0800

    chore: 更换了目录

---
+++ src/data/nav-items.ts
@@ -0,0 +1,10 @@
+export const navItems = [
+  { name: '主页', url: '/' },
+  { name: '禅', url: '/zen/' },
+  { name: '博客', url: '/blog/' },
+  { name: '书签', url: '/bookmarks/' },
+]
+
+export const desktopNavItems = navItems
+
+export const mobileNavItems = navItems.filter((item) => item.name !== '主页')
--- src/utils/nav-items.ts
+++
@@ -1,9 +0,0 @@
-export const navItems = [
-  { name: "主页", url: "/" },
-  { name: "禅", url: "/zen/" },
-  { name: "博客", url: "/blog/" },
-];
-
-export const desktopNavItems = navItems;
-
-export const mobileNavItems = navItems.filter((item) => item.name !== "主页");

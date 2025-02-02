0904e1e72e2cc87eaf9f7b77142974fcc167928f
leungsekyu <leungsekyu@qq.com>
2025-02-03 02:48:05 +0800

    refactor: 将主题颜色存起来

---
+++ src/data/colors.ts
@@ -0,0 +1,18 @@
+export const colors: readonly string[] = [
+  '#c084fc', // bg-purple
+  '#f472b6', // bg-pink
+  '#fb7185', // bg-rose
+  '#e879f9', // bg-fuchsia
+  '#a78bfa', // bg-violet
+  '#818cf8', // bg-indigo
+  '#60a5fa', // bg-blue
+  '#38bdf8', // bg-sky
+  '#22d3ee', // bg-cyan
+  '#2dd4bf', // bg-teal
+  '#34d399', // bg-emerald
+  '#4ade80', // bg-green
+  '#a3e635', // bg-lime
+  '#facc15', // bg-yellow
+  '#fb923c', // bg-orange
+  '#f87171', // bg-red
+]

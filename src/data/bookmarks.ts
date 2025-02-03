dcefb60211d4e52afd5abc9d3e617786de6eb5af
leungsekyu <leungsekyu@qq.com>
2025-02-03 20:56:10 +0800

    improve: 允许书签链接换行

--- src/data/bookmarks.ts
+++ src/data/bookmarks.ts
@@ -27,48 +27,42 @@ export const bookmarks: BookmarkGroups = {
     {
       site: 'https://pss-system.cponline.cnipa.gov.cn/conventionalSearch',
       name: '专利检索及分析',
-      tags: ['专利'],
     },
   ],
   Astro: [
     {
       site: 'https://docs.astro.build/zh-cn/getting-started/',
       name: 'Astro → 文档',
-      tags: ['Astro'],
       favicon: `https://leungsekyu.com/icons/bookmarks/astro.svg`,
     },
     {
       site: 'https://docs.astro.build/zh-cn/recipes/external-links/',
       name: 'Astro → Markdown 外部链接图标',
-      tags: ['Astro'],
       favicon: `https://leungsekyu.com/icons/bookmarks/astro.svg`,
     },
     {
       site: 'https://docs.astro.build/zh-cn/recipes/i18n/',
       name: 'Astro → i18n',
-      tags: ['Astro', 'TODO'],
+      tags: ['TODO'],
       favicon: `https://leungsekyu.com/icons/bookmarks/astro.svg`,
     },
     {
       site: 'https://icones.js.org/collection/logos',
-      name: 'Icônes | SVG Logos',
-      tags: ['图标'],
+      name: 'Icônes → SVG Logos',
       favicon: 'https://leungsekyu.com/icons/bookmarks/icônes.svg',
     },
     {
       site: 'https://github.com/eliancodes/brutal',
       name: 'Brutal',
-      tags: ['Brutal'],
     },
     {
       site: 'https://github.com/ElianCodes/brutal-ui/blob/main/src/components/Pill.astro',
       name: 'Brutal UI → Pill',
-      tags: ['Brutal'],
     },
     {
       site: 'https://pagespeed.web.dev/',
       name: 'PageSpeed Insights',
-      tags: ['Google', '网站性能'],
+      tags: ['网站性能'],
       favicon: 'https://www.gstatic.com/pagespeed/insights/ui/logo/favicon_48.png',
     },
   ],
